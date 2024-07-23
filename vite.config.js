4114bb16d3629e51db48988baf6931e5df272e0a
Kamran Kiani <kiani.km@gmail.com>
2024-07-24 01:03:39 +0330

    Add some css

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 // import htmlPurge from "vite-plugin-html-purgecss";
 
+import { defineConfig } from "vite";
+
 // export default {
 //   plugins: [htmlPurge()],
 // };
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        start: "index.html",
+        question: "question.html",
+        // score: "score.html",
+      },
+    },
+  },
+});
